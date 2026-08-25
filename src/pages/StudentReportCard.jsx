31a019e357530999eebe6f5c2511e8f88efa4311
kernyuypatrick702@gmail.com <kernyuypatrick702@gmail.com>
2026-08-25 02:16:47 +0100

    report cards context import mispell

--- src/pages/StudentReportCard.jsx
+++ src/pages/StudentReportCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import { useAuth } from "../../context/authContext";
+import { useAuth } from "../context/authContext";
 import {
   FaPrint,
   FaAward,
